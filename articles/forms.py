8d6ac011257e98e779d9790851f5e02b63c2d765
HaeunChoi <kclab922@gmail.com>
2023-11-06 13:30:08 +0900

    Comment Create 기능 구현

--- articles/forms.py
+++ articles/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 
 class ArticleForm(forms.ModelForm):
@@ -7,4 +7,17 @@ class ArticleForm(forms.ModelForm):
     # 그래서 메타라는 클래스를 하나 더 만들어서, 거기서 설정값들을 지정하는 것.
     class Meta:
         model = Article
+        # Article이라는 클래스가 갖는 모든 설정값들을 출력해줘
         fields = '__all__'
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        # Comment라는 클래스가 갖는 설정값들 중 content만 출력해줘 
+        # 방법1) fields: content만! 
+        # 방법2) exclude: article 제외하고!
+
+        # fields = ('content', )
+        # 여기서 쉼표를 넣는 이유: 얘는 튜플(혹은 리스트 등과 같이 iterable한 객체)인데, 쉼표가 없으면 그냥 값이라고 인식해버림. 
+        exclude = ('article', )
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
-from .forms import ArticleForm
+from .forms import ArticleForm, CommentForm
 
 # Create your views here.
 
@@ -16,9 +16,11 @@ def index(request):
 
 def detail(request, id):
     article = Article.objects.get(id=id)
+    form = CommentForm()
 
     context = {
-        'article': article
+        'article': article,
+        'form': form,
     }
 
     return render(request, 'detail.html', context)
@@ -38,3 +40,34 @@ def create(request):
     }
 
     return render(request, 'form.html', context)
+
+
+def comment_create(request, article_id):
+    # 여기에는 컨텐츠만 저장된 상태
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        # form을 저장 => 추가로 넣어야 하는 데이터를 넣기 위해 잠시 저장 멈춰! 
+        # 파이썬 코드로는 만들어놓고, db에는 아직 넣지 마.
+        comment = form.save(commit=False)
+
+        # # 첫번째 방법(객체를 저장하는 방법)
+        # # article_id를 기준으로 article obj 가져오기 (Read와 같음)
+        # article = Article.objects.get(id=article_id)
+        # # article 컬럼에 추가
+        # 이 방법은 DB를 조회함
+        # comment.article = article
+        # comment.save()
+
+        # 두번째 방법(integer를 저장하는 방법)
+        # 이 방법은 DB를 호출하지 않으므로, 속도가 더 빠름
+        comment.article_id = article_id
+        comment.save()
+
+        return redirect('articles:detail', id=article_id)
+
+    # else:
+    #     context = {
+    #         'form': form
+    #     }
+    #     return render(request, )
