9aced6eab1f0a0be93dfe5b459774c7e738721e3
HaeunChoi <kclab922@gmail.com>
2023-11-06 13:59:01 +0900

    Comment Create 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    # comment_set => Article과 Comment가 foreign key로 연결되면서 자동생성
 
 class Comment(models.Model):
     content = models.TextField()
@@ -11,3 +12,4 @@ class Comment(models.Model):
     # foreign key: 다대일 (게시물-댓글) 구조일 때 사용
     # on delete: 게시물 자체가 지워졌을 때, 그 영향이 폭포처럼 아래로 내려간다는 것 > 게시물 삭제시 댓글도 다 삭제
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # article.id => Article과 Comment가 foreign key로 연결되면서 자동생성
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Article
+from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 
 # Create your views here.
@@ -18,9 +18,28 @@ def detail(request, id):
     article = Article.objects.get(id=id)
     form = CommentForm()
 
+    # comment 목록 조회 방법 3가지
+
+    # 첫번째 방법
+    # 코멘트를 기준으로 코멘트를 찾는 것
+    # 인스턴스를 직접 찾는 것이므로 article=article로 표기. 이 조건에 부합하는 애들만 필터링해서 찾는 것
+    # comments = Comment.objects.filter(article=article)
+
+    # 두번째 방법
+    # 게시물을 기준으로 해당 게시물에 있는 코멘트를 찾는 것
+    # 여기서 comment_set.() 은 장고 자체적으로 models.py에 클래스들에 자동으로 만들어지는 컬럼
+    # 기준점을 article로 잡아서 예를 들어 1번 게시물로 잡고, 
+    # 파이썬 세상에서 결과물을 도출하고 그것을 html로 연결
+    # comments = article.comment_set.all()
+
+    # 세번째 방법
+    # HTML 코드에서 article.comment_set.all을 사용
+    # HTML 코드에서 파이썬 코드를 실행
+
     context = {
         'article': article,
         'form': form,
+        # 'comments': comments, (3번째 방법에서는 주석처리)
     }
 
     return render(request, 'detail.html', context)
@@ -65,9 +84,3 @@ def comment_create(request, article_id):
         comment.save()
 
         return redirect('articles:detail', id=article_id)
-
-    # else:
-    #     context = {
-    #         'form': form
-    #     }
-    #     return render(request, )
