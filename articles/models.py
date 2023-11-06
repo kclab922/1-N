b2bcb07b91c30e9cdb86119680cb0812cc46d5c8
HaeunChoi <kclab922@gmail.com>
2023-11-06 09:41:54 +0900

    모델링 & 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
+class Comment(models.Model):
+    content = models.TextField()
+    # article이라는 클래스와 연결해주는 id 컬럼을 하나 더 지정
+    # foreign key: 다대일 (게시물-댓글) 구조일 때 사용
+    # on delete: 게시물 자체가 지워졌을 때, 그 영향이 폭포처럼 아래로 내려간다는 것 > 게시물 삭제시 댓글도 다 삭제
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
